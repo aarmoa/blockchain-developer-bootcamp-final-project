2557a2426b2af1770562d05748ea46e281193fce
abel <abel@hummingbot.io>
2021-11-23 00:54:20 -0300

    Change configuration to add .env file with private keys

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 
+require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const MNEMONIC = process.env.MNEMONIC
+const ROPSTEN_URL = process.env.ROPSTEN_URL
+const KOVAN_URL = process.env.KOVAN_URL
+const RINKEBY_URL = process.env.RINKEBY_URL
+const MAINNET_URL = process.env.MAINNET_URL
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -12,6 +22,12 @@ module.exports = {
       host: "localhost",
       port: "8545",
       network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, ROPSTEN_URL)
+      },
+      network_id: 3
     }
   },
   compilers: {
